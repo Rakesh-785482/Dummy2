0edc636a23614c63073e56fcf707752bae27bc5d
Rakesh-785482 <153086111+Rakesh-785482@users.noreply.github.com>
2023-12-06 22:09:57 +0530

    Delete SpringBootWithJsp directory
    This Is

--- SpringBootWithJsp/src/main/java/com/example/repo/JpaRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.entity.Employee;
-
-public interface JpaRepo extends JpaRepository<Employee, Integer> {
-
-}
